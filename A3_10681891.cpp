a9eb05f4c12ead67f89315a28cbb5e98e15dda56
benjamin017 <42819686+benjamin017@users.noreply.github.com>
2018-09-03 06:17:03 +0000

    Add files via upload

---
+++ A3_10681891.cpp
@@ -0,0 +1,50 @@
+ # include <iostream >
+ using namespace std;
+ int main (           )    
+ {
+ 	double grade;
+ 	cout<<"then your grade?";
+ 	cin >>grade
+ 	if ( (  grade >=9 )&&(grade<100)  )  {
+ 		
+ 	cout <<"your letter is grade is ;"A";	
+ 	}
+ 
+ 
+ else if ( ( grade >=80 ) &&(grade <=90) )   
+ {
+ cout <<"your letter grade is ;'<<"B";
+ {
+ 	
+ else if ((grade >=70)%%(grade <=800 ) {
+ cout <<"your letter grade is : :<< "c";	
+ }
+ 
+ 
+ else if ( ( grade >=70)&&(grade <=80 ) ) { 
+ cout <<"your letter grade is:"C":
+ }
+ 
+ {
+else if (  (grade >=60)&&(grade <=70)  ) {
+	
+cout <<"your letter grade is :"D"");	
+}
+
+
+	else if ( (grade >=0) &&(grade<60 ) ) { 
+		cout <<"your letter grade is "<<"E";
+	}
+
+	{
+		else
+		{
+			cout <<"you have been sacked";	
+		}
+	return 0;
+	
+	}
+ 
+
+ 
+
